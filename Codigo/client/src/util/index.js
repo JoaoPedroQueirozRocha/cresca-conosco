083063c455adf433b5bbcd8b4d133d873972f1d7
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2024-04-11 12:09:23 -0300

    feat: add datepicker selection

--- Codigo/client/src/util/index.js
+++ Codigo/client/src/util/index.js
@@ -1,3 +1,8 @@
 export function formartCurrency(value) {
     return '$' + value.toFixed(2);
 }
+
+export function upperCaseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
