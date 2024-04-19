c29c7168db362b8fed9fe46070ce956fa7ef9c90
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2024-04-19 19:29:03 -0300

    fix: fix download

--- Codigo/client/src/util/index.js
+++ Codigo/client/src/util/index.js
@@ -33,3 +33,18 @@ export function getColor(status) {
     }
 }
 
+export function csvExport(data, name) {
+   let content = 'data:text/csv;charset=utf-8,';
+   content += [
+        Object.keys(data[0]).join(','), ...data.map((item) => Object.values(item).join(','))
+    ]
+    .join('\n')
+    .replace(/(^\[)|(\]$)/gm, '');
+
+    const eData = encodeURIComponent(content);
+    const link = document.createElement('a');
+    link.setAttribute('href', eData);
+    link.setAttribute('download',  `${name || 'export'}.csv`);
+    link.click();
+}
+
