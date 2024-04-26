f37bd4a9bda2f52c91df8d1f00bc34c69b4729e1
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2024-04-26 09:47:28 -0300

    minor changes

--- Codigo/client/src/view/funcionarios/components/CreateEdit.vue
+++ Codigo/client/src/view/funcionarios/components/CreateEdit.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="flex justify-between items-center gap-2 mb-8" style="margin-top: 1.5em;">
-        <h2 class="title">{{ title }}</h2>
-    </div>
-    <Tab v-model="tabIndex" :items="tabItems" disabled v-if="!id" class="mb-6" />
+	<div class="flex justify-between items-center gap-2 mb-8" style="margin-top: 1.5em">
+		<h2 class="title">{{ title }}</h2>
+	</div>
+	<Tab v-model="tabIndex" :items="tabItems" disabled v-if="!id" class="mb-6" />
 
-    <div>
-        <Card class="flex flex-col gap-4 mb-4">
-            <Input type="text" label="Nome" v-model="data.nome" placeholder="Digite aqui"></Input>
-            <Input type="text" label="Descrição" v-model="data.descricao" placeholder="Digite aqui"></Input>
+	<div>
+		<Card class="flex flex-col gap-4 mb-4">
+			<Input type="text" label="Nome" v-model="data.nome" placeholder="Digite aqui"></Input>
+			<Input type="text" label="Descrição" v-model="data.descricao" placeholder="Digite aqui"></Input>
 
-            <div class="flex gap-6 flex-wrap">
-                <Input class="md:w-[49%] w-full" type="number" label="Salário" placeholder="Digite aqui" v-model="data.salario" />
-                
-            </div>
-        </Card>
+			<div class="flex gap-6 flex-wrap">
+				<Input
+					class="md:w-[49%] w-full"
+					type="number"
+					label="Salário"
+					placeholder="Digite aqui"
+					v-model="data.salario"
+				/>
+			</div>
+		</Card>
 
-        <div class="flex flex-row flex-wrap gap-2 justify-end ">
-            <Button class="only-border" :disabled="loading" @click="$router.push('/funcionarios')">Cancelar</Button>
-            <Button @click = "salvar" :loading="loading">{{ buttonText }}</Button>
-        </div>
-    </div>
+		<div class="flex flex-row flex-wrap gap-2 justify-end">
+			<Button class="only-border" :disabled="loading" @click="$router.push('/funcionarios')">Cancelar</Button>
+			<Button @click="salvar" :loading="loading">{{ buttonText }}</Button>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -35,150 +40,142 @@ import DatePicker from "@/components/DatePicker.vue";
 import { upperCaseFirstLetter } from "@/util";
 
 export default {
-    name: "CreateEdit",
-    props: {
-        id: String | Number,
-        value: String,
-        types: Array,
-        callback: Function,
-        get: Function,
-    },
-    components: { Button, Dialog, Card, Input, Tab, Checkbox, Select, DatePicker },
-    inject: ["Auth"],
+	name: "CreateEdit",
+	props: {
+		id: String | Number,
+		value: String,
+		types: Array,
+		callback: Function,
+		get: Function,
+	},
+	components: { Button, Dialog, Card, Input, Tab, Checkbox, Select, DatePicker },
+	inject: ["Auth"],
 
-    setup() {
-        const data = ref({
-            nome:'',
-            descricao: '',
-            tipo: '',
-            salario: 0,
-            updated_at: '',
-        });
-        const tabItems = ref([
-            {
-                text: 'Criação',
-                icon: 'add_circle'
-            },
-            {
-                text: 'Importação de Planilha',
-                icon: 'swap_vert'
-            },
-        ]);
-        const tabIndex = ref(0);
-        const tipo = ref("");
-        const defaultAlert = ref({
-            top: true,
-            right: true,
-            timeout: 3500,
-        });
-        const loading = ref(false);
+	setup() {
+		const data = ref({
+			nome: "",
+			descricao: "",
+			tipo: "",
+			salario: 0,
+			updated_at: "",
+		});
+		const tabItems = ref([
+			{
+				text: "Criação",
+				icon: "add_circle",
+			},
+			{
+				text: "Importação de Planilha",
+				icon: "swap_vert",
+			},
+		]);
+		const tabIndex = ref(0);
+		const tipo = ref("");
+		const defaultAlert = ref({
+			top: true,
+			right: true,
+			timeout: 3500,
+		});
+		const loading = ref(false);
 
-        return {
-            data,
-            tipo,
-            tabItems,
-            tabIndex,
-            defaultAlert,
-            loading,
-        }
-    },
+		return {
+			data,
+			tipo,
+			tabItems,
+			tabIndex,
+			defaultAlert,
+			loading,
+		};
+	},
 
-    computed: {
-        title() {
-            if (this.id) return 'Editar ' + upperCaseFirstLetter(this.value);
-            return 'Criar ' + upperCaseFirstLetter(this.value);
-        },
-        buttonText() {
-            if (this.id) return 'Salvar';
-            return 'Criar';
-        }
-    },
+	computed: {
+		title() {
+			if (this.id) return "Editar " + upperCaseFirstLetter(this.value);
+			return "Criar " + upperCaseFirstLetter(this.value);
+		},
+		buttonText() {
+			if (this.id) return "Salvar";
+			return "Criar";
+		},
+	},
 
-    async created() {
-        if (this.id) {
-            try {
-                const { data } = await this.get(Number(this.id));
-                this.data = data;
-                this.tipo = this.types.find((item) => item.value == data.tipo);
-
-            } catch (e) {
-                this.$alert({
+	async created() {
+		if (this.id) {
+			try {
+				const { data } = await this.get(Number(this.id));
+				this.data = data;
+				this.tipo = this.types.find((item) => item.value == data.tipo);
+			} catch (e) {
+				this.$alert({
 					message: `Erro ao requisitar a ${this.value}`,
 					...this.defaultAlert,
 				});
-            }
-        }
-    },
+			}
+		}
+	},
 
-    methods: {
-        async salvar() {
-            this.data.tipo = this.tipo.value;
-            if (!this.isValid()) {
-                this.$alert({
+	methods: {
+		async salvar() {
+			this.data.tipo = this.tipo.value;
+			if (!this.isValid()) {
+				this.$alert({
 					message: `Preencha todos os dados para salvar a ${this.value}`,
 					...this.defaultAlert,
 				});
-                return;
-            }
+				return;
+			}
 
-            this.loading = true;
+			this.loading = true;
 
-            try {
-                if (this.id) {
-                    await this.callback(this.id, this.data);
-                } else {
-                    await this.callback(this.data);
-                }
+			try {
+				if (this.id) {
+					await this.callback(this.id, this.data);
+				} else {
+					await this.callback(this.data);
+				}
 
-                this.$alert({
-                    type: 'success',
+				this.$alert({
+					type: "success",
 					message: `${upperCaseFirstLetter(this.value)} salva com sucesso`,
 					...this.defaultAlert,
 				});
-
-            } catch (e) {
-                console.log(e)
-                this.$alert({
-					message: `Erro ao salvar a ${this.value}. Tente novamente mais tarde`,
+			} catch (e) {
+				console.log(e);
+				this.$alert({
+					message: `Erro ao salvar ${this.value}. Tente novamente mais tarde`,
 					...this.defaultAlert,
 				});
-            } finally {
-                this.loading = false;
-                this.$router.push('/funcionarios');
-            }
-        },
+			} finally {
+				this.loading = false;
+				this.$router.push("/funcionarios");
+			}
+		},
 
-        isValid() {
-            let valid = 
-                this.data.salario > 0 &&
-                this.data.descricao &&
-                this.data.nome;
-            
-            return valid;
-        }
-    },
+		isValid() {
+			return this.data.salario > 0 && this.data.descricao && this.data.nome;
+		},
+	},
 };
 </script>
-
 
 <style lang="scss" scoped>
 @import "../../../style/var.scss";
 
 .selecionar-data {
-    margin-top: 1em;
-    margin-bottom: 1em;
+	margin-top: 1em;
+	margin-bottom: 1em;
 }
 
 .tipo-holder {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    gap: 0.2em;
-    height: fit-content;
-    width: 50%;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	gap: 0.2em;
+	height: fit-content;
+	width: 50%;
 }
 
 .card {
-    margin-bottom: 1%;
+	margin-bottom: 1%;
 }
 </style>
