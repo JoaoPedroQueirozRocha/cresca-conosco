0a8b15bb715e4f65b588fc9886191f0096df8b1c
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2024-04-27 19:19:00 -0300

    feat: creation of import routes

--- Codigo/server/controller/importController.js
+++ Codigo/server/controller/importController.js
@@ -1,6 +1,18 @@
 import * as importServices from '../services/importServices.js';
 
 async function insertMultiple(req, res) {
-    const { headers, data } = req.body;
-    const { tableName } = req.
+    try {
+        const { headers, data } = req.body;
+        const { tableName } = req.params;
+
+        const insert = await importServices.insertMultiple(headers, data, tableName);
+        res.status(200).json(insert);
+    } catch (e) {
+        console.error(e);
+        res.status(e.status).send(e.message);
+    }
+}
+
+export {
+    insertMultiple
 }
--- Codigo/server/services/gestacaoServices.js
+++ Codigo/server/services/gestacaoServices.js
@@ -6,6 +6,11 @@ const router = express.Router();
 
 router.use(express.json());
 
+/**
+ * @param {number} animal_id - O ID do animal para recuperar os dados de gestação.
+ * @returns {Promise<Gestacao>} Uma promessa que resolve para os dados de gestação do animal especificado.
+ * Recupera um registro de gestação pelo ID do animal.
+ */
 async function getGestacaoById(animal_id) {
     const queryResult = await pool.query(
         "SELECT * FROM gestacoes WHERE animal_id = $1",
@@ -14,6 +19,11 @@ async function getGestacaoById(animal_id) {
     return queryResult.rows[0];
 }
 
+/**
+ * @param {object} body - O corpo da requisição com os dados de gestação.
+ * @returns {Promise<Gestacao>} Uma promessa que resolve para os dados de gestação do animal especificado.
+ * Cria um novo registro de gestação.
+ */
 async function createGestacao(body) {
     const { animal_id, status, prev_parto, touro, data_insem, data_finalizacao } = body;
     const queryText = "INSERT INTO gestacoes (animal_id, status, prev_parto, touro, data_insem, data_finalizacao) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
@@ -21,6 +31,12 @@ async function createGestacao(body) {
     return result.rows[0];
 }
 
+/**
+ * @param {number} id - O ID do registro de gestação a ser atualizado.
+ * @param {object} updates - O objeto com as atualizações a serem feitas no registro de gestação.
+ * @returns {Promise<Gestacao>} Uma promessa que resolve para os dados de gestação do animal especificado.
+ * Atualiza um registro de gestação existente.
+ */
 async function updateGestacao(id, updates) {
     const fields = Object.keys(updates)
         .map((field, index) => `${field} = $${index + 1}`)
@@ -32,6 +48,11 @@ async function updateGestacao(id, updates) {
     return result.rows[0];
 }
 
+/**
+ * @param {number} animal_id - O ID do animal para excluir o registro de gestação.
+ * @returns {Promise<Gestacao>} Uma promessa que resolve para os dados de gestação do animal especificado.
+ * Exclui um registro de gestação pelo ID do animal.
+ */
 async function deleteGestacao(animal_id) {
     const result = await pool.query(
         "DELETE FROM gestacoes WHERE animal_id = $1",
