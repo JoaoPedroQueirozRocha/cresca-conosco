d5f69a5b29f85354cbfabb5bab6067963e75b9c5
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2024-05-08 10:34:26 -0300

    fix routes name

--- Codigo/server/services/userServices.js
+++ Codigo/server/services/userServices.js
@@ -11,22 +11,22 @@ async function getUserByEmail(email) {
 }
 
 async function getUserByToken(token) {
-    const queryResult = await pool.query("SELECT * FROM usuario WHERE email = $1", [token])
+    const queryResult = await pool.query("SELECT * FROM usuarios WHERE email = $1", [token])
     return queryResult.rows[0];
 }
 
 async function createNewUser(body) {
-    const result = await pool.query('INSERT INTO usuario (nome, email, token, descricao) VALUES ($1,$2,$3,$4) RETURNING *', [body.nome, body.email, body.token, body.description])
+    const result = await pool.query('INSERT INTO usuarios (nome, email, token, descricao) VALUES ($1,$2,$3,$4) RETURNING *', [body.nome, body.email, body.token, body.description])
     return result;
 }
 
 async function updateUser(body) {
-    const result = await pool.query('UPDATE usuario SET nome = $1, email = $2, token = $3,  descricao = $4 WHERE email = $2', [body.nome, body.email, body.token, body.description])
+    const result = await pool.query('UPDATE usuarios SET nome = $1, email = $2, token = $3,  descricao = $4 WHERE email = $2', [body.nome, body.email, body.token, body.description])
     return result;
 }
 
 async function deleteUser(body) {
-    const result = await pool.query('DELETE FROM usuario WHERE email = $1', [body.email])
+    const result = await pool.query('DELETE FROM usuarios WHERE email = $1', [body.email])
     return result;
 }
 
