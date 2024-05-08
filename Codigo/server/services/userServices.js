b67c74ca5786515a471d21cedf0914d1c8666286
JoaoPedroQueirozRocha <joaopedroqueirozrocha2004@gmail.com>
2024-05-08 11:40:23 -0300

    corrections

--- Codigo/server/services/userServices.js
+++ Codigo/server/services/userServices.js
@@ -6,27 +6,37 @@ const router = express.Router()
 router.use(express.json())
 
 async function getUserByEmail(email) {
-    const queryResult = await pool.query("SELECT * FROM usuario WHERE email = $1", [email])
+    const queryResult = await pool.query("SELECT * FROM usuarios WHERE email = $1", [email])
     return queryResult.rows[0];
 }
 
 async function getUserByToken(token) {
-    const queryResult = await pool.query("SELECT * FROM usuario WHERE email = $1", [token])
+    const queryResult = await pool.query("SELECT * FROM usuarios WHERE email = $1", [token])
     return queryResult.rows[0];
 }
 
 async function createNewUser(body) {
-    const result = await pool.query('INSERT INTO usuario (nome, email, token, descricao) VALUES ($1,$2,$3,$4) RETURNING *', [body.nome, body.email, body.token, body.description])
+    const result = await pool.query('INSERT INTO usuarios (nome, email, token, descricao) VALUES ($1,$2,$3,$4) RETURNING *', [body.nome, body.email, body.token, body.description])
     return result;
 }
 
-async function updateUser(body) {
-    const result = await pool.query('UPDATE usuario SET nome = $1, email = $2, token = $3,  descricao = $4 WHERE email = $2', [body.nome, body.email, body.token, body.description])
-    return result;
+async function updateUser(id, updates) {
+
+    const fields = Object.keys(updates)
+        .map((field, index) => `${field} = $${index + 1}`)
+        .join(", ");
+
+    const values = Object.values(updates);
+
+    const query = `UPDATE usuarios SET ${fields} WHERE id = $${values.length + 1
+        } RETURNING *`;
+    const result = await pool.query(query, [...values, id]);
+    return result.rows[0];
+
 }
 
 async function deleteUser(body) {
-    const result = await pool.query('DELETE FROM usuario WHERE email = $1', [body.email])
+    const result = await pool.query('DELETE FROM usuarios WHERE email = $1', [body.email])
     return result;
 }
 
